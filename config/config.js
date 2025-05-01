d9950a9a953bccaf5e341904317f247f37569dcb
masa.sai <masa.1985@icloud.com>
2025-05-02 06:48:20 +0900

    Fix CI: Use test environment for migrations

--- config/config.js
+++ config/config.js
@@ -12,12 +12,12 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    username: process.env.DB_USERNAME || 'postgres',
+    username: process.env.DB_USERNAME || 'user',
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME_TEST || 'my_api_test',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'postgres',
+    database: process.env.DB_NAME || 'myapp',
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
