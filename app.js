51413b1ea44acac16e428d46bdd17a5984a9ddd6
masa.sai <masa.1985@icloud.com>
2025-05-04 18:40:15 +0900

    CORSに本番URL追加（Render対応）

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config();
 
+// CORS ミドルウェアを最初に配置
 app.use(cors({
   origin: [
     'http://localhost:5173',                     // ローカル開発用
@@ -10,10 +10,12 @@ app.use(cors({
   ],
   credentials: true
 }));
+
 app.use(express.json());
 
-// /users のルートはこれ1つだけで十分！
+// ルートの定義
 const userRoutes = require('./routes/users');
 app.use('/users', userRoutes);
+
 
 module.exports = app;
