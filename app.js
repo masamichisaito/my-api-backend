75a3ae1b1fd6df9fbed6f5b5734637f355c88549
masa.sai <masa.1985@icloud.com>
2025-05-04 16:56:15 +0900

    不要な空のテストファイルを削除

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ require('dotenv').config();
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 
-// テスト用ルート（必ず最後のルートの前に！）
-app.use('/__test__', require('./routes/test'));
-
 // /users のルートはこれ1つだけで十分！
 const userRoutes = require('./routes/users');
 app.use('/users', userRoutes);
