4b3635a1538b0997f86ca367f34c627aa33cb90f
masa.sai <masa.1985@icloud.com>
2025-05-06 12:12:04 +0900

    E2Eテスト修正と安定化

--- config/config.js
+++ config/config.js
@@ -18,12 +18,12 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host:     process.env.DB_HOST,
-    port:     process.env.DB_PORT || 5432,
-    dialect:  'postgres',
     dialectOptions: {
       ssl: {
         require: true,
